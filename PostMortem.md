c7c12a38658abcc5a9e6bb278086c41b67a45d46
Keith Osayande <keithosayande@gmail.com>
2021-08-29 16:07:37 -0400

    Initial commit.

---
+++ PostMortem.md
@@ -0,0 +1,3 @@
+# Post Mortem (React)
+
+I made the decsion to use a React-table library and Material-UI to try and speed up my development process. I have never used either before and in hindsight I probaly shouldn't have, as this slowed down my development. I was only able to make data appear in the table. No interactions were done.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, { useMemo } from "react";
+import moment from "moment";
+import styled from 'styled-components'
+import Table from "./components/Table";
+import data from "./data/candidates.json";
+import {colorMapping} from './utils/enums'
 
+const Styles = styled.div`
+  .dot {
+  height: 25px;
+  width: 25px;
+  border-radius: 50%;
+}
+`
 function App() {
+  const transformData = (data) => {
+    const transformedData = data.results.map((canidate) => {
+      return {
+        name: canidate.name,
+        label: canidate.applications[0].new_status.label,
+        color: canidate.applications[0].new_status.color,
+        appNumber: canidate.applications.length,
+        lastAction: moment([canidate.profile.updated]).fromNow(),
+      };
+    });
+    return transformedData;
+  };
+
+  const formattedData = transformData(data);
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Canidate Name",
+        accessor: "name",
+      },
+      {
+        Header: "Status",
+        accessor: "label",
+        Cell: ({ cell }) => (
+          <div>
+            <span class="dot"></span>
+            {cell.row.values.label}
+          </div>
+        ),
+      },
+      {
+        Header: "# Apps",
+        accessor: "appNumber",
+      },
+      {
+        Header: "Last Action",
+        accessor: "lastAction",
+      },
+    ],
+    []
+  );
+
   return (
-    <header>
-      <p>
-        Edit <code>App.js</code> and save to reload
-      </p>
-    </header>
+    <Styles>
+    <div>
+      <Table columns={columns} data={formattedData} />
+    </div>
+    </Styles>
   );
 }
 
